2291af18382f91c09806fc0eed9c24af735f6895
Clara <solozinhauiui69@gmail.com>
2026-05-25 11:18:35 -0300

    Comecando orientacao a objeto

---
+++ lib/conta.rb
@@ -0,0 +1,24 @@
+class Conta
+
+  attr_reader :numero, :titular, :saldo
+
+  def initialize(numero, titular)
+  @numero = numero
+  @titular = titular
+  @saldo = 0
+  end
+
+  def depositar valor
+    @saldo += valor
+  end  
+   
+  def sacar valor
+    return @saldo -= valor  if valor <= @saldo
+      puts "Saldo insuficente"
+  end  
+  def transferir destino,valor
+    self.sacar valor
+    destino.depositar valor
+  end
+end
+
---
+++ main.rb
@@ -0,0 +1,19 @@
+require_relative "lib/conta"
+require_relative "lib/conta_corrente"
+
+joaoConta = ContaCorrente.new 1, "Joao Barbosa"
+mariaConta = Conta.new 2,"Maria Clara" 
+
+joaoConta.depositar 1000
+joaoConta.depositar 500
+
+mariaConta.depositar 220
+
+joaoConta.sacar 300
+
+mariaConta.sacar 50
+
+joaoConta.transferir mariaConta, 400
+
+puts"#{joaoConta.titular} - Saldo R$ #{ '%.2f' % joaoConta.saldo}"
+puts"#{mariaConta.titular} - Saldo R$ #{ '%.2f' % mariaConta.saldo}"
